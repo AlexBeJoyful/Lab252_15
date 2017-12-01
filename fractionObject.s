e4cfbdb8f3d2a312255afc48fbead866532742e8
AlexBeJoyful <elkinsalexander@gmail.com>
2017-12-01 11:25:26 -0800

    first commit

---
+++ fractionObject.s
@@ -0,0 +1,8 @@
+@ fractionObject.s
+@ field name definitions; requires 8 bytes
+@ 2017-09-29: Bob Plantz
+
+@ fraction object definition
+        .equ    num,0    @ numerator
+        .equ    den,4    @ denominator
+
